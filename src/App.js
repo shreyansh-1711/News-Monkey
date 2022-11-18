ea05db96de70464d12ac922f35c3e54176ea61bb
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-11-18 17:18:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavBar />
+
+      </div>
+    )
+  }
+}
+
+
