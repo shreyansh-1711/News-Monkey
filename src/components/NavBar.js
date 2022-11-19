3639d9dca770d63b17c2f5b3177008842bd8fa0c
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-11-19 23:12:43 +0530

    Update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,9 +18,9 @@ export class NavBar extends Component {
       <li className="nav-item active">
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
-      {/* <li className="nav-item">
+      <li className="nav-item">
         <a className="nav-link" href="#">Link</a>
-      </li> */}
+      </li>
     
   
     </ul>
