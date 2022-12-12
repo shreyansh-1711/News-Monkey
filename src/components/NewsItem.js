f398d526c11bd3ca66e080936b27a39fc0afd3fb
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-12-12 11:55:49 +0530

    refactoring from class to functional based

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,24 +1,24 @@
 // import { getByTitle } from '@testing-library/react'
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let {title, description, imageUrl, newsUrl, author, date} = this.props;
-        return (
-            <div>
-                <div className="card" >
-                    <img src={!imageUrl?"https://images.news18.com/ibnlive/uploads/2022/11/upi-166816266716x9.jpg":imageUrl} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title"> {title}</h5>
-                            <p className="card-text">{description}</p>
-                            <p class="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+const NewsItem = (props) => {
 
-                            <a href={newsUrl} rel="noreferrer" target= "_blank" className="btn btn-sm btn-primary">Read More</a>
-                        </div>
+    let { title, description, imageUrl, newsUrl, author, date } = props;
+    return (
+        <div>
+            <div className="card" >
+                <img src={!imageUrl ? "https://images.news18.com/ibnlive/uploads/2022/11/upi-166816266716x9.jpg" : imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title"> {title}</h5>
+                    <p className="card-text">{description}</p>
+                    <p class="card-text"><small class="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+
+                    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
-        )
-    }s
+        </div>
+    )
+
 }
 
 export default NewsItem
